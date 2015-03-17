fac0305a0261ad5aab2f9b5519f42c465c22d260
KOUYA Welly Arnold <welly.arnold@jighi-ci.com>
2015-03-17 18:38:55 +0100

    adding starting template, in building

---
+++ application/controllers/Crud_generator.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Crud_generator extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('crud_generator_home_view');
+	}
+}
